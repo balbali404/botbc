6a96cc7e60304f43732a9a3226da4d1a78cc00b9
saif22S <48858414+saif22S@users.noreply.github.com>
2019-04-17 20:00:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,31 +22,6 @@ if (message.content.startsWith(prefix + 'help')) { //DiamondCodes - [ X_KillerYT
 ♤?bc2 / بردكست للاونلاين مع مانشن غير مطور
 ♤?bc3 / بردكست للكل مع مانشن غير مطور 
  
- 
-        const backwards = msg.createReactionCollector(backwardsFilter, { time: 2000000});
-        const forwards = msg.createReactionCollector(forwardsFilter, { time: 2000000});
- 
- 
- 
-        backwards.on('collect', r => {
-            if (page === 1) return;
-            page--;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        forwards.on('collect', r => {
-            if (page === pages.length) return;
-     
-      page++;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        })
-    })
-    }
-});
 
 
 
